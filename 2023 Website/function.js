89ed06e7dc5e698b617c24f9b0fb60b51bedaf6a
BrodyBrownlee <86207485+BrodyBrownlee@users.noreply.github.com>
2023-02-28 14:20:55 +1300

    Added Animation to Menu
    also moved around grid and added a title to the home page

--- 2023 Website/function.js
+++ 2023 Website/function.js
@@ -1,15 +1,17 @@
 var pressed = 0;
 var ActiveElement = document.ActiveElement;
-var MyElement = document.getElementById('dropdown'); 
+var MyElement = document.getElementById("dropdown"); 
 function menubutton(){
     if(pressed == 0)
     {
         document.getElementById("dropdown").classList.add("showDropdown");
+        document.getElementById("dropdown").classList.remove("removeDropdown");
         pressed = 1;
     }
     else
     {
         document.getElementById("dropdown").classList.remove("showDropdown");
+        document.getElementById("dropdown").classList.add("removeDropdown");
         pressed=0;
     }
 }
@@ -23,4 +25,6 @@ else{
     pressed = 0;
 }
 
+let thing = document.getElementById("thinggye");
+thing.style.height = thing.style.width;
 
